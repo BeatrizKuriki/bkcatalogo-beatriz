d4fd6af1d100f0f5e70e4bee273ff670e31fa763
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-09-13 11:41:23 -0300

    feat: CategoryResource

--- backend/src/main/java/com/devsuperior/bkcatalogo/entities/Category.java
+++ backend/src/main/java/com/devsuperior/bkcatalogo/entities/Category.java
@@ -1,5 +1,62 @@
 package com.devsuperior.bkcatalogo.entities;
 
-public class Category {
+import java.io.Serializable;
+import java.util.Objects;
+
+
+//Serializable é um padrao da linguagem java para que o obj java possa ser convertido em bytes
+//com Serializable o objeto pode ser gravado em arquivos, passa nas redes(**prática muito importante)
+public class Category implements Serializable{
+	
+	
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private String name;
+	
+	public Category() {
+		
+	}
+	
+	public Category(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return Objects.equals(id, other.id);
+	}
+
+
+	
+	
 
 }
---
+++ backend/src/main/java/com/devsuperior/bkcatalogo/resources/CategoryResource.java
@@ -0,0 +1,46 @@
+package com.devsuperior.bkcatalogo.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.bkcatalogo.entities.Category;
+
+/*
+ * Sempre que um recurso tiver a ver com uma entidade a clase será nomeada com o nome da entidade 
+ * mais a palavra Resource- como no caso dessa classe que se chama CategoryResources(sendo a entidade
+ * a classe Category e o recurso a classe CategoryResource) que vai indicar que essa classe é um recurso
+ * da entidade (no caso entidade Category)
+ * Para configurar no Spring que está classe vai ser um controlador REST e vai responder requisições
+ * nesse recurso, a gente colocar uma annotation chamada RestController
+ * O annotation é uma forma enxuta e simples de você configurar alguma coisa no seu código
+ * ao invés de ter de programar do zero vc usa algo que já está implementado
+ * No Spring os programadores do framework já implementaram toda a infraestrutura necessária
+ * para transformar uma classe em um recurso Rest/ essa annotation realiza um pré-processamento por 
+ * baixo dos panos ao compilar a classe e ela será disponibilizada como recurso */
+
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+	
+	// A classe Resource implementa o controlador Rest
+	/*
+	 * O backend vai disponibilizar uma API(Application Programming Interface)
+	 * Como se o recurso fosse um conceito e o controlador a forma de implementar esse conceito
+	 * ABAIXO SERÁ CRIADO O PRIMEIRO ENDPOINT- PRIMEIRA ROTA POSSÍVEL QUE VAI RESPONDER ALGUMA COISA
+	 */
+	@GetMapping
+	public ResponseEntity<List<Category>>findAll(){
+		List<Category> list = new ArrayList<>();
+		list.add(new Category(1L, "Books"));
+		list.add(new Category(2L, "Eletronics"));
+		return ResponseEntity.ok().body(list);
+		
+	}
+
+}
