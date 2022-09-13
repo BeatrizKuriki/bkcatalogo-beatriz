f5fbe63b5efccc5d4558f6b5cf171f19a749477e
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-09-13 10:14:43 -0300

    feat: backend project created

---
+++ backend/src/main/java/com/devsuperior/bkcatalogo/BkcatalogoApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.bkcatalogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkcatalogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BkcatalogoApplication.class, args);
+	}
+
+}
