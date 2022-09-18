470a0b800ba01e75957eca441cae7f8a2bd5355c
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-09-18 12:04:35 -0300

    feat: Auditing data

--- backend/src/main/java/com/devsuperior/bkcatalogo/entities/Category.java
+++ backend/src/main/java/com/devsuperior/bkcatalogo/entities/Category.java
@@ -1,12 +1,16 @@
 package com.devsuperior.bkcatalogo.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 
@@ -26,7 +30,42 @@ public class Category implements Serializable{
 	private String name;
 	
 	
+	/*
+	 * SEMPRE QUE FOR ARMAZENAR DADOS QUE PRECISAM DE HORAS E DATAS DÊ PREFERÊNCIA PARA TIMEZONE UTC E DEPOIS TRATE A RESPOSTA DE ACORDO
+	 * COM O LOCAL EM QUE O SERVIÇO SERÁ PRESTADO OU EXIBIDO.
+	 */
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant creatAt;
 	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updateAt;
+	
+	
+	
+	public Instant getUpdateAt() {
+		return updateAt;
+	}
+
+
+
+	public Instant getCreatAt() {
+		return creatAt;
+	}
+	
+
+	@PrePersist
+	public void prePersist() {
+		creatAt = Instant.now();
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		updateAt = Instant.now();
+	}
+	
+
+	
+
 	public Category() {
 		
 	}
